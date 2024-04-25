5f49300c2e68cbdbc8a7726978c904c6e5c37c25
gourisnair1 <156325450+gourisnair1@users.noreply.github.com>
2024-04-25 13:51:15 +0000

    s

--- app.py
+++ app.py
@@ -9,16 +9,18 @@ def hello():
 @app.route('/prediction', methods=['GET','POST'])
 def predict():
     if request.method == 'POST':
+        gender = request.form['gender']
+        print(gender)
         age = request.form['age']
         print(age)
         salary = request.form['salary']
         print(salary)
-        female = request.form['female']
-        print(female)
-        male = request.form['male']
-        print(male)
-        model = pickle.load(open('model (1).pkl','rb'))
-        purchased = model.predict([[float(age),float(salary),float(female),float(male)]])
+        # female = request.form['female']
+        # print(female)
+        # male = request.form['male']
+        # print(male)
+        model = pickle.load(open('ad_model.pkl','rb'))
+        purchased = model.predict([[float(gender),float(age),float(salary)]])
         print(purchased[0])
 
     return render_template('prediction.html', purchased = (purchased[0]))
